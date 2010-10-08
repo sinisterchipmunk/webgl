b30405f3d37b2f2258315b23a88231007ff8cad0
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2010-10-08 19:30:20 -0400

    Tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 RailsGame::Application.routes.draw do
-  routes = proc do
-    match '/:controller/:action'
-  end
-  
-  scope "/~colin/rails-game" do
-    instance_eval &routes
-    #instance_eval &(Auth.routing_proc)
-  end
-
-  instance_eval &routes
+  match '/:controller/:action'
+  root :controller => 'engine_tests', :action => 'index'
 end
